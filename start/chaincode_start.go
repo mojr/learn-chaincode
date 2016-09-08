fc35fa42e350ce562d39773b7a018fd7f2d60577
Marko Modsching <marko.modsching@dser.de>
2016-09-08 14:21:33 +0200

    .

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -112,7 +112,7 @@ func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) 
 	}
 	
 	key = args[0]
-	valAsbytes, err := stub.getState(key)
+	valAsbytes, err := stub.GetState(key)
 	if err != nil {
 		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
 		return nil, errors.New(jsonResp)
